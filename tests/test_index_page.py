8df8b0a2a4f17965e5d52fba32dd118cba78c5fa
Сэр Дъмитрий <altynd@mail.ru>
2023-02-06 16:45:20 +0400

    end of pytest

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,4 +1,4 @@
-import time
+# import time
 
 import pytest
 import pages
@@ -8,4 +8,6 @@ class TestFooter:
 
     def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
         pages.index_page.open_index_page(page)
-        time.sleep(10)
+        # time.sleep(10)
+        actual_result = pages.index_page.get_text_from_google_search_button(page)
+        assert actual_result == "Поиск в Google"
