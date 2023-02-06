3dc64b86d541fd9b168abcffaa03a27689a615f6
Сэр Дъмитрий <altynd@mail.ru>
2023-02-06 16:45:20 +0400

    end of pytest

--- pages/index_page.py
+++ pages/index_page.py
@@ -3,6 +3,10 @@ import config
 
 
 class IndexPage:
+    _BUTTON_GOOGLE_SEARCH = "//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']"
 
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
+
+    def get_text_from_google_search_button(self, page: Page)-> None:
+        return page.locator(self._BUTTON_GOOGLE_SEARCH).get_attribute('value')
